d486123d3f9a48da93a3322151b1ad30b9dd81b5
jatinvyas1 <jatin.vyas@neewee.ai>
2022-12-04 08:37:10 +0000

    completed the assessment

---
+++ src/Timer.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+
+const Timer = () => {
+  const [isActive, setIsActive] = useState(false);
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isActive) {
+      intervalId = setInterval(() => {
+        setCounter((counter) => counter + 1);
+      }, 1000);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [isActive, counter]);
+
+  function stopTimer() {
+    setIsActive(false);
+    setCounter(0);
+  }
+
+  return (
+    <div class="container">
+      <div class="time">{counter}</div>
+      <div class="buttons">
+        <button onClick={() => setIsActive(!isActive)} class="start">
+          {isActive ? "Pause" : "Start"}
+        </button>
+        <button onClick={stopTimer} class="reset">
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Timer;
